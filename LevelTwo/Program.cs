e6a771f32fe360080869efb698a6ac59779b8b69
Joao A <joao.pedro.matias.amaral@gmail.com>
2026-03-17 14:17:55 +0000

    add leveltwo, tux
    made the program work, was checking for empty args instead of empty
    length, added both images

---
+++ LevelTwo/Program.cs
@@ -0,0 +1,29 @@
+﻿using Spectre.Console;
+
+namespace LevelTwo
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            CanvasImage tux = new CanvasImage("tux.png");
+
+            tux.MaxWidth(20);
+
+
+            if(args.Length == 0)
+            {
+                AnsiConsole.Write(tux);
+            }
+            else
+            {
+                CanvasImage image = new CanvasImage(args[0]);
+
+                int imageWidth = int.Parse(args[1]);
+                image.MaxWidth(imageWidth);
+
+                AnsiConsole.Write(image);
+            }
+        }
+    }
+}
