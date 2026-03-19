0577370915a12a87467171c54952d5a94a32db17
Joao A <joao.pedro.matias.amaral@gmail.com>
2026-03-19 16:39:18 +0000

    made the functions and call it in main

--- DungeonStats/Program.cs
+++ DungeonStats/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Spectre.Console;
 
 namespace DungeonStats
 {
@@ -6,7 +7,41 @@ namespace DungeonStats
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            int damageInt = int.Parse(args[0]);
+            int defenseInt = int.Parse(args[1]);
+
+            Console.WriteLine(Damage(damageInt,defenseInt));
+        }
+
+        private static int Damage(int attack, int defense)
+        {
+            if(defense < 0)
+            {
+                return attack;
+            }
+            else
+            {
+                return attack - defense;
+            }
+        }
+
+        private static int Damage(int attack)
+        {
+            return attack;
+        }
+
+        private static int CriticalHit(int damage)
+        {
+            int crit = 0;
+            if(damage <= 0)
+            {
+                return 0;
+            }
+            else
+            {
+                crit = 1 - Damage(damage);
+            }
+            return crit;
         }
     }
 }
